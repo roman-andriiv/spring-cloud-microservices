409c796b8f96d4b28de22cf0660c1a2c3e4bbcb4
Roman Andriiv <roman.andriyiv@gmail.com>
2023-07-18 22:26:36 +0200

    [andriiv][CCS] Using Feign REST Client for Service Invocation

--- currency-conversion-service/src/main/java/com/andriiv/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/andriiv/currencyconversionservice/CurrencyConversionController.java
@@ -15,6 +15,11 @@ import java.util.Map;
  */
 @RestController
 public class CurrencyConversionController {
+    private final CurrencyExchangeProxy proxy;
+    public CurrencyConversionController(CurrencyExchangeProxy proxy) {
+        this.proxy = proxy;
+    }
+
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
                                                           @PathVariable String to,
@@ -31,6 +36,19 @@ public class CurrencyConversionController {
         return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
                 currencyConversion.getConversionMultiple(),
                 quantity.multiply(currencyConversion.getConversionMultiple()),
-                currencyConversion.getEnvironment());
+                currencyConversion.getEnvironment() + " " + "rest template" );
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from,
+                                                               @PathVariable String to,
+                                                               @PathVariable BigDecimal quantity) {
+
+        CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
+
+        return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment() + " " + "feign");
     }
 }
---
+++ currency-conversion-service/src/main/java/com/andriiv/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,15 @@
+package com.andriiv.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Created by Roman Andriiv (18.07.2023 - 22:22)
+ */
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
