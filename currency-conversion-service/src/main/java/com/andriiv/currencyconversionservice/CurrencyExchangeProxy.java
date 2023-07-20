e1bd410fb6d159015a220149d735e21b35999948
Roman Andriiv <roman.andriyiv@gmail.com>
2023-07-20 11:16:09 +0200

    [andriiv][AG] Add and setting up Spring Cloud API Gateway

--- currency-conversion-service/src/main/java/com/andriiv/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/andriiv/currencyconversionservice/CurrencyExchangeProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * Created by Roman Andriiv (18.07.2023 - 22:22)
  */
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
