4d2d92dd8c51e9a8f655dc614f87a04f175bd754
Roman Andriiv <roman.andriyiv@gmail.com>
2023-07-22 21:57:07 +0200

    [andriiv][AG]  Implementing Spring Cloud Gateway Logging Filter

---
+++ api-gateway/src/main/java/com/andriiv/apigateway/LoggingFilter.java
@@ -0,0 +1,24 @@
+package com.andriiv.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by Roman Andriiv (22.07.2023 - 21:46)
+ */
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
